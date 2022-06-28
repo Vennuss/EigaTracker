3d633b0dfbe6bf911b98899672d6c1d5ba2d819e
Hugo <huchanserio@gmail.com>
2022-06-28 19:45:20 +0200

    Conexion a BD en la codificación.

---
+++ Codificación/EigaTracker/src/eigatracker/Bd.java
@@ -0,0 +1,17 @@
+package eigatracker;
+
+/**
+ *
+ * @author Vennuss
+ */
+public class Bd {
+    
+    final private String URL = "jdbc:mysql://localhost:3306/mysql?zeroDateTimeBehavior";
+    final private String USER = "root";
+    final private String PASSWORD = "";
+    
+    public void Sentencia(final String _sql){
+        
+    }
+    
+}
